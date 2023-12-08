8fb7cca7c79dba39c45346738137ac4cd5c4a181
neilmenon <steelneil12@gmail.com>
2023-12-08 15:31:58 -0500

    Updating CD/CD config creation

---
+++ src/app/config/config.secrets.ts
@@ -0,0 +1,16 @@
+export const config = {
+    spotify: {
+        clientId: "${{ secrets.SPOTIFY_CLIENT_ID }}",
+        authCodeUrl: "${{ secrets.AUTH_CODE_URL }}",
+        redirectUri: "${{ secrets.SPOTIFY_REDIRECT_URI }}",
+        scopes: {
+            "spotify-album-sort": "${{ secrets.SPOTIFY_SCOPES_LIBRARY_SORT }}",
+            "anniversify": "${{ secrets.SPOTIFY_SCOPES_ANNIVERSIFY }}"
+        },
+    },
+    anniversify: {
+        apiRoot: "${{ secrets.ANNIVERSIFY_API_ROOT }}",
+        emailSender: "${{ secrets.ANNIVERSIFY_EMAIL_SENDER }}",
+        vapidPublicKey: "${{ secrets.VAPID_PUBLIC_KEY }}",
+    },
+}
